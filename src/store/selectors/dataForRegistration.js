7841af9edfdfe63091e842035b3dbed0566857b3
daysg <days.grace2015@yandex.ru>
2022-09-16 18:38:57 +0300

    refactor: delete the store fix: fix user registration

--- src/store/selectors/dataForRegistration.js
+++
@@ -1,7 +0,0 @@
-import { useSelector } from "react-redux";
-
-export const getFullName = () => useSelector((state) => state.dataForRegistrationReducer.FullName);
-export const getEmail = () => useSelector((state) => state.dataForRegistrationReducer.Email);
-export const getPassword = () => useSelector((state) => state.dataForRegistrationReducer.Password);
-export const getConfirmPassword = () =>
-  useSelector((state) => state.dataForRegistrationReducer.ConfirmPassword);
