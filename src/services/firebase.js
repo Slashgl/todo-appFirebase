93bd8cf935aba18b74329f63023763b4431906f9
daysg <days.grace2015@yandex.ru>
2022-09-19 21:37:30 +0300

    refactor: create separate file for api key

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCDsFJASUMSverRpe_ARWPsUavbow7zmL4",
-  authDomain: "fir-registration-5ec0c.firebaseapp.com",
-  databaseURL: "https://fir-registration-5ec0c-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fir-registration-5ec0c",
-  storageBucket: "fir-registration-5ec0c.appspot.com",
-  messagingSenderId: "253234909171",
-  appId: "1:253234909171:web:9e6b38324ccf8acf3e8c86",
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
 });
 
 const firestore = firebase.firestore();
