86548fcdee57de2b68e5a82d145ab33a3756eef9
daysg <days.grace2015@yandex.ru>
2022-09-20 12:12:52 +0300

    refactor: create folder for utils and services

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { firestore } from "./services/firebase";
+import { firestore } from "./services/firebase/firebase";
 import { Registration } from "components";
 import { logoShape } from "./assets";
 import styles from "./styles.module.scss";
 
 const App = () => {
   return (
-    <div className={styles.wrapper}>
+    <>
       <img className={styles.img} src={logoShape} alt="img" />
       <Registration firestore={firestore} />
-    </div>
+    </>
   );
 };
 
