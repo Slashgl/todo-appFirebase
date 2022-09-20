b7f39a59ba98b2f3aa409ffcb5ffcd5d169378fe
daysg <days.grace2015@yandex.ru>
2022-09-20 11:46:01 +0300

    refactor: move img

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { firestore } from "./services/firebase";
 import { Registration } from "components";
+import { logoShape } from "./assets";
+import styles from "./styles.module.scss";
 
 const App = () => {
-  return <Registration firestore={firestore} />;
+  return (
+    <>
+      <img className={styles.img} src={logoShape} alt="img" />
+      <Registration firestore={firestore} />
+    </>
+  );
 };
 
 export default App;
