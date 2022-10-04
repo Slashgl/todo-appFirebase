c0eefc22df7ea9719b4c716b65f37f9925c5d4a8
daysg <days.grace2015@yandex.ru>
2022-10-04 22:35:24 +0300

    refactor: correct code and delete folder

--- src/components/homePage/index.jsx
+++ src/components/homePage/index.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { useAuth } from "hooks";
-import { useNavigate } from "react-router-dom";
+import { getUser } from "store";
 
 const HomePage = () => {
-  const { isAuth, email } = useAuth();
-  const navigate = useNavigate();
-  return (isAuth && <div>Welcome {email}</div>) || navigate("/");
+  const user = getUser();
+  return <div>Welcome {user.email}</div>;
 };
 
 export default HomePage;
