44324e3124e946dfbd97779cbacddbb2c8330b74
daysg <days.grace2015@yandex.ru>
2022-10-05 11:47:20 +0300

    refactor: fix alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Registration } from "components";
-import HomePage from "./components/homePage";
-import { firestore } from "services/firebase";
-import Login from "./components/login";
-import NewPassword from "./components/newPassword";
+import { HomePage, Registration, Login, NewPassword } from "components";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Registration firestore={firestore} />} />
+      <Route path="/" element={<Registration />} />
       <Route path="/homePage" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/reset-password" element={<NewPassword />} />
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Registration from "./registration";
 import HomePage from "./homePage";
 import ButtonSubmitForm from "./registration/buttonSubmitForm";
+import Login from "./login";
+import NewPassword from "./newPassword";
 
-export { Registration, HomePage, ButtonSubmitForm };
+export { Registration, HomePage, ButtonSubmitForm, Login, NewPassword };
