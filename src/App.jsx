1ad4ee87751b890e192eb98ceed55799d016f3e7
daysg <days.grace2015@yandex.ru>
2022-10-05 11:50:00 +0300

    refactor: add constant for routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage, Registration, Login, NewPassword } from "components";
 
 const App = () => {
+  const register = "/";
+  const homePage = "/homePage";
+  const login = "/login";
+  const newPassword = "/reset-password";
+
   return (
     <Routes>
-      <Route path="/" element={<Registration />} />
-      <Route path="/homePage" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/reset-password" element={<NewPassword />} />
+      <Route path={register} element={<Registration />} />
+      <Route path={homePage} element={<HomePage />} />
+      <Route path={login} element={<Login />} />
+      <Route path={newPassword} element={<NewPassword />} />
     </Routes>
   );
 };
