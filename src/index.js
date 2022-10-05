82194ba71f3c620e0979729c0d534ca07b37fbb5
daysg <days.grace2015@yandex.ru>
2022-10-05 14:06:32 +0300

    refactor: correct import sorting

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./store";
 import "services/firebase";
 import App from "./App";
-import { store } from "./store";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
