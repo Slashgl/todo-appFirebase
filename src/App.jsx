485498b897d3d55ce4ee49e46f88e8076924abbe
daysg <days.grace2015@yandex.ru>
2022-10-21 14:00:26 +0300

    refactor: fix import style App

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import { Login, NewPassword, Registration } from "components";
 import { HomePage } from "pages";
 import ROUTES from "routes";
+import styles from "./styles.module.scss";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path={ROUTES.HOME} element={<HomePage />} />
-      <Route path={ROUTES.REGISTER} element={<Registration />} />
-      <Route path={ROUTES.LOGIN} element={<Login />} />
-      <Route path={ROUTES.NEW_PASSWORD} element={<NewPassword />} />
-    </Routes>
+    <div className={styles}>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.REGISTER} element={<Registration />} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+        <Route path={ROUTES.NEW_PASSWORD} element={<NewPassword />} />
+      </Routes>
+    </div>
   );
 };
 
