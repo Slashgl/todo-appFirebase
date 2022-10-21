5309b133e97e97d36ba8286710c40f52b063fa08
daysg <days.grace2015@yandex.ru>
2022-10-21 13:00:43 +0300

    refactor: create separate page for registration, logIn, newPassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Login, NewPassword, Registration } from "components";
-import { HomePage } from "pages";
+import { RegistrationPage, LogInPage, NewPasswordPage, HomePage } from "pages";
 import ROUTES from "routes";
 
 const App = () => {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<HomePage />} />
-      <Route path={ROUTES.REGISTER} element={<Registration />} />
-      <Route path={ROUTES.LOGIN} element={<Login />} />
-      <Route path={ROUTES.NEW_PASSWORD} element={<NewPassword />} />
+      <Route path={ROUTES.REGISTER} element={<RegistrationPage />} />
+      <Route path={ROUTES.LOGIN} element={<LogInPage />} />
+      <Route path={ROUTES.NEW_PASSWORD} element={<NewPasswordPage />} />
     </Routes>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 import HomePage from "./homePage";
+import RegistrationPage from "./registrationPage";
+import LogInPage from "./logInPage";
+import NewPasswordPage from "./newPasswordPage";
 
-export { HomePage };
+export { HomePage, RegistrationPage, NewPasswordPage, LogInPage };
